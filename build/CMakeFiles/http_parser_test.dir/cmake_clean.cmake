275a0f8ef2b66107c223de2d17c144b398937288
Dasti-dev <20cs3015@rgipt.ac.in>
2025-12-28 00:57:25 +0000

    Implement streaming HTTP reverse proxy with correct epoll dispatch

---
+++ build/CMakeFiles/http_parser_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/http_parser_test.dir/src/protocol/http/http_parser.cpp.o"
+  "CMakeFiles/http_parser_test.dir/src/protocol/http/http_parser.cpp.o.d"
+  "CMakeFiles/http_parser_test.dir/tests/unit/http_parser_test.cpp.o"
+  "CMakeFiles/http_parser_test.dir/tests/unit/http_parser_test.cpp.o.d"
+  "http_parser_test"
+  "http_parser_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/http_parser_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
